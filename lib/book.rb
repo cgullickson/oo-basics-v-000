edf654fb0619bcb798b75923657d59c8349e275a
Chris Gullickson <cgullickson5@gmail.com>
2017-11-28 20:49:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,12 +4,12 @@ class Book
   def initialize(title)
     @title = title
   end
-  
+
   def title
     @title
   end
 
-  def author = (author)
+  def author (author)
     @author = author
-    
+
 end
